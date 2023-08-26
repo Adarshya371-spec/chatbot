90d7999c6fcb54922cc6edee024c4fccf4b14d74
Adarshya <77096378+Adarshya371-spec@users.noreply.github.com>
2023-08-26 12:19:08 +0530

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,43 @@
+#Text Data Preprocessing Lib
+import nltk
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+import pickle
+import numpy as np
+
+words=[]
+classes = []
+word_tags_list = []
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+train_data_file = open('intents.json').read()
+intents = json.loads(train_data_file)
+
+# function for appending stem words
+def get_stem_words(words, ignore_words):
+   
+for intent in intents['intents']:
+    
+        # Add all words of patterns to list
+       
+
+
+#Create word corpus for chatbot
+def create_bot_corpus(stem_words, classes):
+
+    stem_words = sorted(list(set(stem_words)))
+    classes = sorted(list(set(classes)))
+
+    pickle.dump(stem_words, open('words.pkl','wb'))
+    pickle.dump(classes, open('classes.pkl','wb'))
+
+    return stem_words, classes
+
+stem_words, classes = create_bot_corpus(stem_words,classes)  
+
+print(stem_words)
+print(classes)
+
+     
